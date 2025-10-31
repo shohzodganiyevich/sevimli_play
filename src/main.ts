407cc9c65950c6c9bfbdb39929107e12eab60b48
Shohzod Ganiyevich <shohzodganiyevich@gmail.com>
2025-10-31 05:16:34 +0500

    Docker-3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  const config = app.get(ConfigService);
+  const PORT = config.get<number>('PORT') ?? 3030;
+  await app.listen(PORT, () => {
+    console.log(`Server started at:http://localhost:${PORT}`);
+  });
 }
-bootstrap();
+start();
